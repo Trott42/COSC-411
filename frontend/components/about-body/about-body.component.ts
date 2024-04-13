2713f803d5d84865d684a50d09bc8b7a48ad6c6e
lthacke3 <lthacke3@emich.edu>
2024-04-13 14:06:47 -0400

    renamed to just "frontend"

---
+++ frontend/components/about-body/about-body.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-body',
+  standalone: true,
+  imports: [],
+  templateUrl: './about-body.component.html',
+  styleUrl: './about-body.component.scss'
+})
+export class AboutBodyComponent1 {
+
+}
