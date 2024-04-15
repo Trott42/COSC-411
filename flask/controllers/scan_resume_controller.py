8a6459b6167473d55ae1512ca9907e7b43d5f4c3
lthacke3 <lthacke3@emich.edu>
2024-04-15 15:05:51 -0400

    the controller should be able to take in the resume data (in theory)

--- flask/controllers/scan_resume_controller.py
+++ flask/controllers/scan_resume_controller.py
@@ -3,6 +3,6 @@ from services.scan_resume_service import scan_resume
 
 scan_resume_controller = Blueprint("scan_resume_controller", __name__)
 
-@scan_resume_controller.route("/scan-results", methods=['GET'])
-def scan_results():
-    return scan_resume()
+@scan_resume_controller.route("/scan-results/<str:resume>", methods=['GET', 'POST'])
+def scan_results(resume: str):
+    return scan_resume(resume)
