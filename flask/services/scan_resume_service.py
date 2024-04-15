60b03c523f273c3d6e93617cc7bc6a2858f21da9
lthacke3 <lthacke3@emich.edu>
2024-04-15 15:22:55 -0400

    it may just work, with a lot of hopes and dreams

--- flask/services/scan_resume_service.py
+++ flask/services/scan_resume_service.py
@@ -1,7 +1,28 @@
 from flask import jsonify
 from utils import Resume
 
-def scan_resume(resume_file: str):
-    response = Resume.Resume(resume_file)
+def calc_score(resume: Resume) -> float:
+    """
+    remember to change this to score every skill listed in the job description
+    """
+    res_dict = resume.extract()
     
-    return jsonify(response)
+    try:
+        skill_score = res_dict["skills"]
+        school_data = res_dict["education"]
+        school_name = school_data.keys()[0]
+        school_score = school_data[school_name]
+        
+        #why is their job dependent on their skills and education? I don't know! Stop asking!
+        return skill_score - school_score
+    except KeyError as e:
+        print("Was not able to properly parse resume data.\n" + e)
+        return 0
+
+def scan_resume(resume_file: str):
+    resume = Resume.Resume(resume_file)
+    resume_score = calc_score(resume)
+    
+    return jsonify({
+        resume.extract(): resume_score
+    })
